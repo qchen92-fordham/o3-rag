606c10742ae450e9bca5216d719aba07ca717138
Qing Chen <qingchen@tesla.com>
2025-12-27 16:01:23 -0800

    add Render

--- app.py
+++ app.py
@@ -3,17 +3,33 @@ from rag_example import search_in_vectorstore
 
 client = OpenAI()
 
-def chat(query):
+def chat(query, chat_history):
     # user_input = input("Enter your message: ")
     # context = search_in_vectorstore(user_input)
     context = search_in_vectorstore(query)
     prompt_with_context = f"Context: {context}\n\nUser input: {query}"
 
+    # save the conversation history
+    chat_history.append({"role": "user", "content": prompt_with_context})
+
     response = client.chat.completions.create(
         model="o3-mini-2025-01-31",
-        messages=[{"role": "user", "content": prompt_with_context}],
+        messages=chat_history,
+        stream=True, #stream response, like ChatGPT generates live response
     )
 
-    return response.choices[0].message.content
+    # store the response in chat history
+    complete_response = ""
 
-print(chat("Where is the closest bike station? I live near Berlin Ostbahnhof."))
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            complete_response += chunk.choices[0].delta.content
+            yield chunk.choices[0].delta.content
+
+# for chunk in chat("Where is the closest bike station? I live near Berlin Ostbahnhof.\
+#                   Please list all locations that are neaby." #if you add this, you will see the result chunk by chunk
+#                   #just like ChatGPT streaming the response.
+#                   ):
+#     print(chunk, end="", flush=True)
+
+    chat_history.append({"role": "assistant", "content": complete_response})
--- rag_server.py
+++ rag_server.py
@@ -1,14 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response, request, jsonify, render_template
 from app import chat
 
 app = Flask(__name__)
 
+@app.route('/')
+def index(): # this is to connect to index webpage
+    return render_template('index.html')
+
+# extend our RAG server, so it has a short-term memory
+# we also have to differentiate requests from different users
+chat_histories = {}
 @app.route('/chat', methods=['POST'])
 def chat_endpoint():
     data = request.json
     query = data['query']
-    response = chat(query)
-    return jsonify({'response': response})
+    session_id = data['session_id']
+
+    if session_id not in chat_histories:
+        chat_histories[session_id] = []
+
+    def stream_with_context(query):
+        for chunk in chat(query, chat_histories[session_id]):
+            yield chunk
+
+    return Response(stream_with_context(query), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=80, host='0.0.0.0')
